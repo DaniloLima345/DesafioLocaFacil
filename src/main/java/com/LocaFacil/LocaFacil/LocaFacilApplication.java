3eec16df76ebd236ab45b1a96d4ff72c5cb563e4
DaniloLima345 <86997053+DaniloLima345@users.noreply.github.com>
2023-08-03 12:48:22 -0300

    Initial commit

---
+++ src/main/java/com/LocaFacil/LocaFacil/LocaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.LocaFacil.LocaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LocaFacil/LocaFacil/LocaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LocaFacil.LocaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
